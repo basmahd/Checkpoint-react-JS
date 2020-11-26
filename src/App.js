5fecdaa1fad33e8b8415419b30a7038bedd12af4
unknown <bsm.hamdi09@gmail.com>
2020-11-26 11:44:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav } from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import {InputGroup} from 'react-bootstrap';
+import {FormControl} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+import React from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+    <Nav fill variant="tabs"defaultActiveKey="/home">
+  <Nav.Item>
+    <Nav.Link href="/home" className="Styl">Gateaux photos</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1"className="Styl">Gateaux à thème</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2"className="Styl">Mini Gateaux</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="disabled"className="Styl">Faire soi-meme</Nav.Link>
+  </Nav.Item>
+</Nav>
+<h1 className="titre">Pour s'inscrire dans notre forum</h1>
+<div className="formulaire">
+
+<Form >
+  <Form.Row className="align-items-center">
+    <Col xs="auto">
+      <Form.Label htmlFor="inlineFormInput" srOnly>
+        Name
+      </Form.Label>
+      <Form.Control
+        className="mb-2"
+        id="inlineFormInput"
+        placeholder="Entrer email"
+      />
+    </Col>
+    <Col xs="auto">
+      <Form.Label htmlFor="inlineFormInputGroup" srOnly>
+        Username
+      </Form.Label>
+      <InputGroup className="mb-2">
+        <InputGroup.Prepend>
+          <InputGroup.Text>@</InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl id="inlineFormInputGroup" placeholder="Username" />
+      </InputGroup>
+    </Col>
+    <Col xs="auto">
+      <Form.Check
+        type="checkbox"
+        id="autoSizingCheck"
+        className="mb-2"
+        label="Remember me"
+      />
+    </Col>
+    <Col xs="auto">
+      <Button type="submit" className="mb-2">
+        Submit
+      </Button>
+    </Col>
+  </Form.Row>
+</Form>
+</div>
     </div>
   );
 }
